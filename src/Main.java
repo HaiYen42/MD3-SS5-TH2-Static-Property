7f319771e3f3ac6f461f7c7210a451f424592bc3
HaiYen42 <haiyentth42@gmail.com>
2023-03-05 23:29:36 +0700

    Car

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+       Car car1= new Car("Vinfast", "VF6");
+        System.out.println(Car.numberOfCars);
+
+        Car car2= new Car("Vinfast", "VF8");
+        System.out.println(Car.numberOfCars);
+
+    }
+}
